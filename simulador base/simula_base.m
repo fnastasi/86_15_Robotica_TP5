09747595b1f32c7f345774794fc1bb8a94302a00
Franco <fnastasi@fi.uba.ar>
2019-12-14 18:27:04 -0300

    Se agregaron las funciones pinvScara e interpoladorTrapezoidal

--- simulador base/simula_base.m
+++ simulador base/simula_base.m
@@ -13,12 +13,10 @@ RPM = 2*pi/60;
 % Instrucciones de movimiento
 % **************************************************************
 % Matriz de puntos destinos. Los puntos destino se agregan como columnas a la matriz
-pdest=[0        -0.6999     1 1;
-       0.5576   0.2553      1 1;
-       0        -0.6999     1 1;
-       0        -0.6999     1 1]';
+pdest=[-300       300     0 1;
+       300        300     0 1]';
 % Los tiempos deseados de cada movimiento se ingresan en un vector columna
-TDeseado=[0 1 0.5 1 1];
+TDeseado=[0.1];
 
 % **************************************************************
 % Parametros de la simulacion
@@ -44,8 +42,8 @@ tacc=100E-3;	% [seg]
 % **************************************************************
 % Parametros del controlador
 % **************************************************************
-Kp=[7000 0;0 2000]
-Kd=[300 0;0 25]
+Kp=[7000 0;0 2000];
+Kd=[300 0;0 25];
 
 
 % **************************************************************
